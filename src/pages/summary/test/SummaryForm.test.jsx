eaa85c1023f881a6019d22842e912aba5fcd3b65
Javiera Fortunato <javiera.fortunato@perficient.com>
2023-04-04 15:45:43 -0400

    SummaryForm component and test finished

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 describe("SummaryForm", () => {
@@ -11,15 +12,32 @@ describe("SummaryForm", () => {
     expect(checkbox).not.toBeChecked();
     expect(button).toBeDisabled();
   });
-  test("Should enable button on first click of checkbox, and disable on the second click", () => {
+  test("Should enable button on first click of checkbox, and disable on the second click", async () => {
     render(<SummaryForm />);
+    const user = userEvent.setup();
     const checkbox = screen.getByRole("checkbox", {
       name: /terms and conditions/i,
     });
     const button = screen.getByRole("button", { name: /confirm order/i });
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(button).toBeEnabled();
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(button).toBeDisabled();
+  });
+  test("Should render a popover only when cursor is over Terms and Conditions text", async () => {
+    render(<SummaryForm />);
+    const user = userEvent.setup();
+    const text = screen.getByText(/terms and conditions/i);
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+    await user.hover(text);
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+    await user.unhover(text);
+    expect(popover).not.toBeInTheDocument();
   });
 });
